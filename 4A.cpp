bdddb3e442e3f56de48321632ecf10b7c6b2cd37
tolamson2811 <tolamson2811@gmail.com>
2022-03-09 21:56:32 +0700

    4A

--- 4A.cpp
+++ 4A.cpp
@@ -6,10 +6,20 @@ void test(int arr[SIZE]) {
         arr[i] = 0;
     }
 }
+void testString(string s) {
+    s = "Thaydoi";
+}
 int main() {
     int arr[SIZE] = {1, 2, 3, 4, 5};
+    test(arr);
     for(int i = 0; i < SIZE; i++) {
         cout << arr[i] << " ";
     }
-    // In ra 1 2 3 4 5 => mảng truyền theo cơ chế pass-by-value
+    cout << endl;   
+    // In ra 0 0 0 0 0 => mảng truyền theo cơ chế pass-by-reference
+    string s = "Baitap";
+    testString(s);
+    cout << s;
+    // In ra "Baitap" => string truyền theo cơ chế pass-by-value
+    return 0;
 }
